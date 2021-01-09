1af9b79f613434ef28e3f11c43f7c34a9233758a
Erik Hanchett <icystorm@gmail.com>
2021-01-09 15:53:57 -0800

    First commit pokemon vue app

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div
+      v-if="pokemon"
+      className="w-3/12 m-auto bg-purple-100 mt-4 shadow-2xl flex justify-center flex-col items-center"
+    >
+      <h3 className="text-2xl text-green-900 uppercase">{{ pokemon.name }}</h3>
+      <div class="flex justify-center">
+        <img className="w-48" :src="pokemon.sprites.front_shiny" alt="" />
+        <img className="w-48" :src="pokemon.sprites.back_shiny" alt="" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { reactive, toRefs } from "vue";
+import { useRoute } from "vue-router";
+
+export default {
+  setup() {
+    const route = useRoute();
+
+    const pokemon = reactive({
+      pokemon: ""
+    });
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        pokemon.pokemon = data;
+        console.log(data);
+      });
+    return { ...toRefs(pokemon) };
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mt-10 p-4 flex flex-wrap">
+    <div
+      class="ml-4 text-2xl text-blue-400"
+      v-for="(pokemon, idx) in pokemons"
+      :key="idx"
+    >
+      <router-link :to="`/about/${idx + 1}`">
+        {{ pokemon.name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { reactive, toRefs } from "vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    const pokemonList = reactive({
+      pokemons: []
+    });
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        pokemonList.pokemons = data.results;
+      });
+
+    return { ...toRefs(pokemonList) };
   }
-}
+};
 </script>
